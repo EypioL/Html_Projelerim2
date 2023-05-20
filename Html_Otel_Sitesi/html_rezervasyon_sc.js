a9c1051fcc847c5d8352942aeaae978121c70ff5
EypioL <aposamet0@gmail.com>
2023-05-20 10:10:37 +0300

    Otel

---
+++ Html_Otel_Sitesi/html_rezervasyon_sc.js
@@ -0,0 +1,4 @@
+function uyari(){
+    alert("Rezervasyon Bilgileriniz Kaydedildi:)");
+ }
+ //function kodunda oclick olarak verdiğim uyariya burda alert vererek kaydet butonuna basıldığında alert verdirdim;//
